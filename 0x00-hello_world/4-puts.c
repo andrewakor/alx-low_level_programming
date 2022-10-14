f6be57863c3229489c9e842370a43d1fddf841c5
Andrew <akorandrew91@gmail.com>
2022-10-13 22:10:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  * Return: Always 0
  */
 int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
+ { 
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return(0);
-}
+ }
